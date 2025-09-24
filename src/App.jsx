07bce874c6f3a60e028f967fb27c259554fc6f17
Subhashini <subhashinikumar2004@gmail.com>
2025-09-24 21:21:43 +0530

    connected Login with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
+    <AuthProvider>
     <Router>
       <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard/>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </Layout>
     </Router>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,61 @@
+import { createContext, useContext, useState, useEffect } from "react";
+const API_URL = import.meta.env.VITE_API_URL;
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    const savedToken = localStorage.getItem("token");
+
+    if (savedUser && savedToken) {
+      setUser(JSON.parse(savedUser));
+      setToken(savedToken);
+    }
+    setLoading(false);
+  }, []);
+
+  const login = async (email, password) => {
+    const formData = { email, password };
+    try {
+      const res = await fetch(`${API_URL}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!res.ok) {
+        return false;
+      }
+      const data = await res.json();
+      setToken(data.token);
+      localStorage.setItem("token",data.token);
+      setUser(data.user);
+      localStorage.setItem("user",JSON.stringify(data.user));
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
